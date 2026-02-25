d07452564e31d3a71e0ab46a1cefb956b043f618
MartinsX7 <goncalvesmartins5454@gmail.com>
2026-02-24 23:08:01 -0300

    Fazendo pagina de login

--- pizzaria-FULLSTACK/src/components/Login.jsx
+++ pizzaria-FULLSTACK/src/components/Login.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 
 const Login = () => {
+    
 
     return(
-        <div>
+        <div className='card-login'>
             <h1>Login</h1>
             <label>Username:</label>
             <input type="text"  placeholder="Digite seu usuario"/>
             <label>Senha:</label>
             <input type="password" placeholder="Digite sua senha" />
             <button>Entrar</button>
-            <p>Não tem cadastro?<a href="">Sing up</a></p>
+            <p>Não tem cadastro?<a href="">Cadastre-se</a></p>
         </div>
     )
 }
