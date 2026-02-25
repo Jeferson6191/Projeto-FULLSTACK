98c2433589bc8b3a32283f7c95df287390fcaa88
Jeferson6191 <jeffrodjs0@gmail.com>
2026-02-24 22:26:57 -0300

    criando primeira rota

--- pizzaria-FULLSTACK/src/main.js
+++ pizzaria-FULLSTACK/src/main.js
@@ -1,3 +1,12 @@
-import express from "express";
-import env
-app.
+import express from "express"
+const app = express()
+import "dotenv/config"
+const PORT = process.env.PORT
+
+app.get("/", (req,res)=>{
+    res.send("Hello World")
+})
+
+app.listen(PORT, () =>{
+    console.log(`porta aberta ${PORT}`)
+})
