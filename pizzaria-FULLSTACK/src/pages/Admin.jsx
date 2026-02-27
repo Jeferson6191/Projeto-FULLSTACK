a713dce10378decfe5c8f796fc02d8734d84a9c6
MartinsX7 <goncalvesmartins5454@gmail.com>
2026-02-26 22:32:11 -0300

    admin.jsx praticamente pronto, falta css e o cliente

--- pizzaria-FULLSTACK/src/pages/Admin.jsx
+++ pizzaria-FULLSTACK/src/pages/Admin.jsx
@@ -1,20 +1,77 @@
-import { logo } from '../assets'
+import { useState, useEffect } from "react";
 import '../style/admin.css'
+
+
+
 
 const Admin = () => {
 
+  const [pedidos, setPedidos] = useState([]);
 
+  useEffect(() => {
+    fetch("http://localhost:3000/pedidos")
+      .then(res => res.json())
+      .then(data => setPedidos(data));
+  }, []);
+
+  async function finalizarPedido(id) {
+    await fetch(`http://localhost:3000/pedidos/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ status: "Finalizado" })
+    });
+
+    // Atualiza na tela depois
+    setPedidos(pedidos.map(p =>
+      p.id == id ? { ...p, status: "Finalizado" } : p
+    ));
+  }
 
     return (
         <div className="container">
             <header className="header">
-                <img src={logo} alt="" />
+                <img src="/img/logo.png" alt="" />
             </header>
             <div class="parent">
-                <div class="div1"> </div>
+                <div class="div1">
+                    <h3>Dashboard</h3>
+                    <h3>Pedidos</h3>
+                    <h3>Cardapio</h3>
+                    <h3>Usuarios</h3>
+                </div>
             </div>
 
-            <main className='~main-container'>
+            <main className='main-container'>
+                <div className="card-main">
+                    <div className="card-pedidos">Pedidos Hoje</div>
+                    <div className="card-faturamento">Faturamento do dia</div>
+                    <div className="card-andamento">Em andamento</div>
+                    <div className="card-finalizado">Finalizados</div>
+                </div>
+
+                <section className="gerenciamento-pedidos">
+
+                </section>
+                <hr />
+                
+                <section className="cardapio">
+                    <div className="btn-alinhado">
+                        <button>+ Adicionar Pizza</button>
+                        <button>Editar Pizza</button>
+                        <button>Remover Pizza</button>
+                    </div>
+                    <div className="table-pizza">
+                        <table>
+                            <tr>
+                                <th>Nome da Pizza</th>
+                                <th>Preco</th>
+                                <th>Categoria</th>
+                                <th>Ações</th>
+                            </tr>
+
+                        </table>
+                    </div>
+                </section>
 
             </main>
 
